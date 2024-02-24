808c24bc6ff56e3017fb55103948ad5c881d239c
HajMichal <michalhaj2003@gmail.com>
2024-02-24 15:31:57 +0100

    [Initial]: created basic database schema (tables, relationships), prepared docker for nestJs and mysql server

---
+++ prisma/migrations/20240224125116_init/migration.sql
@@ -0,0 +1,62 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `adress` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `createdat` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `User_id_key`(`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Contractor` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `companyName` VARCHAR(191) NOT NULL,
+    `companyEmail` VARCHAR(191) NOT NULL,
+    `companyAdress` VARCHAR(191) NOT NULL,
+    `nip` VARCHAR(191) NULL,
+    `createdat` DATETIME(3) NOT NULL,
+    `userId` INTEGER NOT NULL,
+
+    UNIQUE INDEX `Contractor_id_key`(`id`),
+    UNIQUE INDEX `Contractor_userId_key`(`userId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Building` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `adress` VARCHAR(191) NOT NULL,
+    `city` VARCHAR(191) NOT NULL,
+    `zipcode` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `estimatedcost` VARCHAR(191) NOT NULL,
+    `userId` INTEGER NOT NULL,
+
+    UNIQUE INDEX `Building_id_key`(`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Booking` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `deadline` DATETIME(3) NOT NULL,
+    `buildingId` INTEGER NOT NULL,
+    `contractorId` INTEGER NULL,
+
+    UNIQUE INDEX `Booking_id_key`(`id`),
+    UNIQUE INDEX `Booking_buildingId_key`(`buildingId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Contractor` ADD CONSTRAINT `Contractor_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Building` ADD CONSTRAINT `Building_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Booking` ADD CONSTRAINT `Booking_buildingId_fkey` FOREIGN KEY (`buildingId`) REFERENCES `Building`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Booking` ADD CONSTRAINT `Booking_contractorId_fkey` FOREIGN KEY (`contractorId`) REFERENCES `Contractor`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
